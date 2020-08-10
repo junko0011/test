5c091c08c4f1839e670ba53611f81d18c4a8ca80
junko0011 <junko0011@naver.com>
2020-08-10 13:10:42 +0900

    login 완성

--- app/src/main/java/com/example/aitest2/LoginResultActivity.java
+++ app/src/main/java/com/example/aitest2/LoginResultActivity.java
@@ -11,7 +11,7 @@ import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginResultActivity extends AppCompatActivity {
 
-    TextView TextViewGet;
+    TextView TextViewGet;/* 2.선언*/
 
 
     @Override
@@ -19,13 +19,13 @@ public class LoginResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);/* 1.화면매칭*/
 
-        TextViewGet=findViewById(R.id.TextViewGet);
+        TextViewGet=findViewById(R.id.TextViewGet);/* 3.값을 찾아오고*/
 
-        Intent intent=getIntent();
-        Bundle bundle = intent.getExtras();/* 엑스트라들을 먼저 가져온뒤 값을 꺼내온다*/
-        String Email = bundle.getString("Email");
+        Intent intent=getIntent();/* 4.intent는 값을 주고받는데 쓰자라는 규약.*/
+        Bundle bundle = intent.getExtras();/* 5.엑스트라들을 번들로 값을 가져온다*/
+        String Email = bundle.getString("Email");/* 6.번들에서 스트링을 가져온다*/
         String Password = bundle.getString("Password");
-        TextViewGet.setText(Email+"/"+Password);/* 불러온것을 출력해줘*/
+        TextViewGet.setText(Email+"/"+Password);/*6.불러온것을 출력해줘*/
 
     }
 }
--- app/src/main/java/com/example/aitest2/MainActivity.java
+++ app/src/main/java/com/example/aitest2/MainActivity.java
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.RelativeLayout;
-import com.google.android.material.textfield.TextInputEditText;
+/*3.전역변수에 대한 import*/
 
+import com.google.android.material.textfield.TextInputEditText;
+import android.widget.RelativeLayout;
 public class MainActivity extends AppCompatActivity {
 
-/*2.지역변수 지정*/
+/*2.전역변수 지정*/
     TextInputEditText TextInputEditTextEmail, TextInputEditTextPassword;
     RelativeLayout RelativeLayout_Login, RelativeLayout_Inroll;
 
@@ -19,12 +20,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);/* 1.화면매칭*/
 
-/*3.지역변수와 소스매칭시키기 (지역변수와 소스 이름 같게하여 편리하게) / activity logind에서 소스 찾아와 이름은 R.id.이거야*/
+/*4.전역변수와 소스매칭시키기 (전역변수와 소스 이름 같게하여 편리하게) / activity login에서 소스 찾아와! 이름은 R.id.이거야*/
         TextInputEditTextEmail=findViewById(R.id.TextInputEditTextEmail);
         TextInputEditTextPassword=findViewById(R.id.TextInputEditTextPassword);
         RelativeLayout_Login=findViewById(R.id.RelativeLayout_Login);
         RelativeLayout_Inroll=findViewById(R.id.RelativeLayout_Inroll);
 
+/*5. 1)값을 가져온다. 2)클릭을 감지한다. 3)1번의 값을 다음 엑티비티(화면)으로 넘긴다.
 
         RelativeLayout_Login.setClickable(true);/*클릭이 가능한 녀석을 세팅한다 / setOnClickListener는 클릭을 감지하면 어떻게 하겠다*/
         RelativeLayout_Login.setOnClickListener(new View.OnClickListener() {
@@ -32,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String Email = TextInputEditTextEmail.getText().toString();/* 텍스트 가져온걸 스트링(문자)으로 바꿔줘*/
                 String Password = TextInputEditTextPassword.getText().toString();
-                /*아래 4개 함수에 의해 스트링받은 걸 보내줘*/
+
+                /*아래 4개 함수에 의해 스트링받은 걸 LoginResultActivity로 넘긴다*/
                 Intent intent=new Intent(MainActivity.this, LoginResultActivity.class); /*intent값을 주고받음 MainActivity 에서 LoginResultActivity로 보내라*/
                 intent.putExtra("Email",Email); /*값 가져온걸 intent로 들고 있어라*/
                 intent.putExtra("Password",Password);
