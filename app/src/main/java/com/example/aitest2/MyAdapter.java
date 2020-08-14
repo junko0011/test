b28eef99690c142bdd7623b95408f08c4403deb7
junko0011 <junko0011@naver.com>
2020-08-15 08:07:59 +0900

    리사이클러뷰 기본 화면구성 완성

--- app/src/main/java/com/example/aitest2/MyAdapter.java
+++ app/src/main/java/com/example/aitest2/MyAdapter.java
@@ -1,3 +1,5 @@
+package com.example.aitest2;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -6,7 +8,6 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
-import com.example.aitest2.R;
 
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
@@ -16,11 +17,12 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
+        //3. MyViewHolder 에서 각 자식들 데이터 지정해주고    /  each data item is just a string in this case
         public TextView TextView_title;
         public TextView TextView_content;
         public ImageView ImageView_title;
         public MyViewHolder(View v) {
+        //4. 자식들 요소들 찾아서 사용해라 View v 에서 찾아야 하므로 v.findVewById 사용
             super(v);
             TextView_title = v.findViewById(R.id.TextView_title);
             TextView_content = v.findViewById(R.id.TextView_content);
@@ -28,7 +30,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         }
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
+    //5. NewsActivity에서 지정해주는 숫자만큼 반복하여 화면에 띄워라    /  Provide a suitable constructor (depends on the kind of dataset)
     public MyAdapter(String[] myDataset) {
         mDataset = myDataset;
     }
@@ -37,10 +39,11 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     @Override
     public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
                                                      int viewType) {
-        // create a new view
+        // 1. onCreateViewHolder에서 한줄 한줄 보여주는 레이아웃 지정 layout.row_news   /  create a new view
         LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.row_news, parent, false);
 
+        // 2. 한줄 레이아웃을 MyViewHolder로 넘겨라
         MyViewHolder vh = new MyViewHolder(v);
         return vh;
     }
@@ -50,7 +53,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public void onBindViewHolder(MyViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.textView.setText(mDataset[position]);
+        holder.TextView_title.setText(mDataset[position]);
 
     }
 
--- app/src/main/java/com/example/aitest2/NewsActivity.java
+++ app/src/main/java/com/example/aitest2/NewsActivity.java
@@ -1,48 +1,40 @@
 package com.example.aitest2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-/*3.전역변수에 대한 import*/
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.textfield.TextInputEditText;
-import android.widget.RelativeLayout;
-public class MainActivity extends AppCompatActivity {
 
-/*2.전역변수 지정*/
-    TextInputEditText TextInputEditTextEmail, TextInputEditTextPassword;
-    RelativeLayout RelativeLayout_Login, RelativeLayout_Inroll;
+public class NewsActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private String[] mDataset = {"1","2"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);/* 1.화면매칭*/
+        setContentView(R.layout.activity_news);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
-/*4.전역변수와 소스매칭시키기 (전역변수와 소스 이름 같게하여 편리하게) / activity login에서 소스 찾아와! 이름은 R.id.이거야*/
-        TextInputEditTextEmail=findViewById(R.id.TextInputEditTextEmail);
-        TextInputEditTextPassword=findViewById(R.id.TextInputEditTextPassword);
-        RelativeLayout_Login=findViewById(R.id.RelativeLayout_Login);
-        RelativeLayout_Inroll=findViewById(R.id.RelativeLayout_Inroll);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
 
-/*5. 1)값을 가져온다. 2)클릭을 감지한다. 3)1번의 값을 다음 엑티비티(화면)으로 넘긴다.
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
-        RelativeLayout_Login.setClickable(true);/*클릭이 가능한 녀석을 세팅한다 / setOnClickListener는 클릭을 감지하면 어떻게 하겠다*/
-        RelativeLayout_Login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String Email = TextInputEditTextEmail.getText().toString();/* 텍스트 가져온걸 스트링(문자)으로 바꿔줘*/
-                String Password = TextInputEditTextPassword.getText().toString();
-
-                /*아래 4개 함수에 의해 스트링받은 걸 LoginResultActivity로 넘긴다*/
-                Intent intent=new Intent(MainActivity.this, LoginResultActivity.class); /*intent값을 주고받음 MainActivity 에서 LoginResultActivity로 보내라*/
-                intent.putExtra("Email",Email); /*값 가져온걸 intent로 들고 있어라*/
-                intent.putExtra("Password",Password);
-                startActivity(intent);
-
-
-            }
-        });
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(mDataset);
+        recyclerView.setAdapter(mAdapter);
     }
+    // ...
+
 }
